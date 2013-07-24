14a0ba1903e407a58c6f79ebcbbbf26172625ab9
Patsy Price <patsy.m.price@gmail.com>
2013-07-23 17:22:43 -0700

    pictures & colors working

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -16,8 +16,25 @@ $(document).ready(function(){
           var box = $(this);
           var color = box.css('background-color'); // getter method
           $('#selected_color').css('background-color', color);
-
       };
+
+      var add_picture = function(){
+        var box = $('<div>');
+        box.addClass('box');
+        var image = $('#picture').val();
+        box.css('background-image', ('url('+image+')'));
+        box.css('background-size', 'cover');
+        $('#pictures').prepend(box);
+        $('#picture').val('');
+      };
+
+      var set_background = function(){
+        var box = $(this);
+        var image = box.css('background-image');
+        $('#canvas').css('background-image', image);
+        $('#canvas').css('background-size', cover);
+      };
+
 
       var paint = function(){
         var paint_box = $(this);
@@ -25,10 +42,15 @@ $(document).ready(function(){
         paint_box.css('background-color', color);
       };
 
+
       $('#add_color').on('click', add_color);
       // delegated the event handled to an element that
       // aready existed on the page #colors div
       $('#colors').on('click', '.box', set_color);
       $('.paint_box').on('mouseover ', paint);
+      $('#add_picture').on('click', add_picture);
+      $('#pictures').on('click', '.box', set_background);
 
 });
+
+// need to add images to painter & add
